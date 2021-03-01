68a6bdb7910e8782fda25548a20a7d0ff00459d1
David Nitu <davidnitu44@gmail.com>
2021-03-01 17:49:49 +0000

    finished tv shows and movies search

--- javascript/script.js
+++ javascript/script.js
@@ -1,34 +1,82 @@
-const searchForm = document.querySelector("#searchForm")
+const showSearch = document.querySelector("#searchForm")
 const searchSubmit = document.querySelector("#searchSubmit");
 const searchInput = document.querySelector("#searchInput");
 const displaySearches = document.querySelector("#displaySearches");
+const filmMode = document.querySelector("#filmMode");
+const movieSearch = document.querySelector("#movieSearch");
+const movies = document.querySelector("#movies");
+const movieInput = document.querySelector("#movieInput")
+const tvShows = document.querySelector("#tvShows");
+const movieList = document.querySelector("#displayMovies")
 
 let data = [];
 
-searchForm.addEventListener("submit", async function (e) {
+
+
+showSearch.addEventListener("submit", async function (e) {
         e.preventDefault();
-        const searchValue = searchForm.elements.searchInput.value;
+        const searchValue = showSearch.elements.searchInput.value;
         
         deleteItems(data);
         data = await fetchShowsAPI(searchValue);
-        displayItems(data);
+        displayShow(data);
         searchInput.value = "";
-        
+
+        console.dir(data);
 });
 
+
+movieSearch.addEventListener("submit", async function (e) {
+        e.preventDefault();
+        const searchValue = movieSearch.elements.movieInput.value;
+
+        deleteItems(data);
+        data = await fetchShowsAPI(searchValue);
+        displayMovie(data);
+        movieInput.value = "";
+
+        console.dir(data);
+});
+
+
+    filmMode.addEventListener("input", async function(e) {
+        
+        if (this.value == "movie") {
+            tvShows.style.display = "none";
+            movies.style.display = "block";
+            deleteItems(data);
+
+        } 
+        else if (this.value == "tvshow") {
+            movies.style.display = "none";
+            tvShows.style.display = "block";
+            deleteItems(data);
+        }
+    })
+   
+
+
+
 const fetchShowsAPI = async (value) =>  {
-    const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${value}`);
-    return res.data;
+    if(filmMode.value == "movie") {
+        const config = {params: {apikey : "37697e59", r: "json", s: value}};
+        const res = await axios.get("http://www.omdbapi.com/", config);
+        return res.data.Search;
+    } else if (filmMode.value == "tvshow") {
+        const res = await axios.get(`http://api.tvmaze.com/search/shows/?q=${value}`);
+        return res.data;
+    }
+
 }
 
-const createItem = (showTitle, showImg, showGenres, showSummary) => {
+const createShow = (showTitle, showImg, showGenres, showSummary) => {
     showTitle.innerText = item.show.name;
     showImg.src = item.show.image.medium;
     showGenres.innerText = item.show.genres;
     showSummary.innerHTML = item.show.summary;
 }
 
-const displayItems = (arrItems) => {
+const displayShow = (arrItems) => {
     if (data.length !== 0) {
         for (item of arrItems) {
             if(item.show.image) {
@@ -38,9 +86,34 @@ const displayItems = (arrItems) => {
                 const genres = document.createElement("span");
                 const summary = document.createElement("p");
 
-                createItem(title, img, genres, summary);
+                createShow(title, img, genres, summary);
                 newItem.append(title, img, genres, summary);
                 displaySearches.append(newItem);
+            }
+        }
+    } else {
+        console.log("NO MATCHES FOUND!");
+    }
+}
+
+const createMovie = (movieTitle, movieImg, movieYear) => {
+    movieTitle.innerText = item.Title;
+    movieImg.src = item.Poster;
+    movieYear.innerText = item.Year;
+}
+
+const displayMovie = (arrItems) => {
+    if (data.length !== 0) {
+        for (item of arrItems) {
+            if(item.Poster != "N/A" && item.Type == "movie") {
+                const newItem = document.createElement("li");
+                const title = document.createElement("h2");
+                const img = document.createElement("img");
+                const year = document.createElement("span");
+
+                createMovie(title, img, year);
+                newItem.append(title, img, year);
+                movieList.append(newItem);
             }
         }
     } else {
@@ -53,4 +126,5 @@ const deleteItems = (arrItems) => {
         arrItems.splice([i]);
     }
     displaySearches.innerHTML = "";
+    movieList.innerHTML = "";
 }
